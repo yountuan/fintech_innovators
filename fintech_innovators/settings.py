a6322cb5febdf70d40afa34f0e9bcfd8299cbefe
yountuan <yountuan@Aidas-MacBook-Air.local>
2024-04-16 20:42:21 +0600

    added host

--- fintech_innovators/settings.py
+++ fintech_innovators/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(',')
+ALLOWED_HOSTS = ['*']
 AUTH_USER_MODEL = 'users.User'
 
 # Application definition
